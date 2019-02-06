d21be6cc019ca97a06488a14c18f1a4efaa0bb06
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-06 11:56:16 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,14 @@
 def hello_t(array)
-  i=0
-  while i<array.length
-    yield
-    i=i+1
-  end
-return array
+  if block_given?
+    i=0
+    while i<array.length
+      yield
+      i=i+1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end  
 end
 
 names = ["Tim", "Tom", "Jim"]
