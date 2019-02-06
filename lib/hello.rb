3301cc57e87bd141acffe22b6e8d56ef96efccd4
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-06 11:28:05 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,7 @@
 def hello_t(array)
   i=0
   while i<array.length
-    yield array[y]
+    yield array[i]
     i+=i
   end
-end
-
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-    if name.start.with?("T")
-      puts "Hi, #{name}"
-    end
 end
